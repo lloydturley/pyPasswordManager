9b18f29169747e147bdff0cca27a08373c75ea27
Lloyd Turley <Lloyd.Turley@hyland.com>
2024-04-23 15:20:26 -0400

    added password search

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-    nr_letters = random.randint(8,10)
-    nr_symbols = random.randint(2,4)
-    nr_numbers = random.randint(2,4)
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
     password = [random.choice(letters) for _ in range(nr_letters)]
     password += [random.choice(numbers) for _ in range(nr_numbers)]
@@ -24,9 +24,8 @@ def generate_password():
     password = ''.join(str(x) for x in password)
     pyperclip.copy(password)
 
-    password_input.delete(0,END)
-    password_input.insert(0,password)
-
+    password_input.delete(0, END)
+    password_input.insert(0, password)
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -60,6 +59,23 @@ def save_password():
             password_input.delete(0, END)
             website_input.focus()
 
+
+# ---------------------------- SEARCH --------------------------------- #
+def find_password():
+    website = website_input.get()
+    try:
+        with open("passwordFile.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(message=f"Email/username: {email}\nPassword: {password}")
+        else:
+            messagebox.showwarning(message="No details for the website exists")
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -81,14 +97,17 @@ password_label = Label(text="Password")
 password_label.grid(row=4, column=0)
 
 # entries
-website_input = Entry(width=35, justify="left")
-website_input.grid(row=2, column=1, columnspan=2, sticky=EW)
+website_input = Entry(width=21, justify="left")
+website_input.grid(row=2, column=1, sticky=EW)
 website_input.focus()
 emun_input = Entry(width=35, justify="left")
 emun_input.grid(row=3, column=1, columnspan=2, sticky=EW)
 emun_input.insert(0, "lloydturley2@gmail.com")
 password_input = Entry(width=21, justify="left")
 password_input.grid(row=4, column=1, sticky=EW)
+
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=2, column=2, sticky=EW)
 
 genpassword_button = Button(text="Generate Password", justify="right", command=generate_password)
 genpassword_button.grid(row=4, column=2, sticky=EW)
